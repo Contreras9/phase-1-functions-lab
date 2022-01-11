3d7fda0cbb0a921de6152022f201c5bf9ea52628
Hamzah Contreras <Hamzah_36@outlook.com>
2022-01-10 21:31:54 -0600

    commit tree a17636d3cf1f4717940b32546102d55fc88baa9e

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 // Code your solution in this file!
 
+
 function distanceFromHqInBlocks(block) {
-    if (block < 42) {
-        return 42 - block;
+    if(block >= 42) {
+        return (block - 42)
     } else {
-        return block - 42;
+        return (42 - block)
     }
 }
 
-function distanceFromHqInFeet(blocknum) {
-    return distanceFromHqInBlocks(blocknum) * 264;
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264
 }
 
-function distanceTravelledInFeet(start, ending) {
-    if (start <= ending) {
-        return (ending - start) * 264; 
+function distanceTravelledInFeet(start, destination) {
+    if(start < destination) {
+        return (destination - start) * 264
     } else {
-        return (start - ending) * 264;
+        return (start - destination) * 264
     }
 }
 
 function calculatesFarePrice(start, destination) {
-    let destinationInFeet = distanceTravelledInFeet(start, destination);
-    if (destinationInFeet <= 400) {
-        return 0;
-    } if (destinationInFeet > 400 && destinationInFeet <= 2000) {
-        return (destinationInFeet - 400) * 0.02;
-    } if (destinationInFeet > 2000 && destinationInFeet <= 2500) {
+    const distanceTravelled = distanceTravelledInFeet(start, destination)
+    if(distanceTravelled <= 400){
+        return 0
+    } else if(distanceTravelled >= 401 && distanceTravelled <= 2000) {
+        return (distanceTravelled - 400) * .02
+    } else if(distanceTravelled >= 2001 && distanceTravelled <= 2500) {
         return 25
     } else {
         return "cannot travel that far"
